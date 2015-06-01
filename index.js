6a294baec949bea76548f99a16148fa8c1a884ea
crossjs <liwenfu@crossjs.com>
2015-06-01 21:09:40 +0800

    update

--- index.js
+++ index.js
@@ -8,8 +8,25 @@
 
 'use strict';
 
-module.exports = function(options) {
-
-  require('./lib/' + options.type)(options)
-
+module.exports = {
+  command: 'init',
+  description: '初始化项目文件',
+  options: [{
+    name: 'force',
+    alias: 'f',
+    description: '直接覆盖，不做目录非空校验',
+    defaults: false
+  }],
+  bootstrap: require('./lib/init'),
+  help: function(chalk) {
+    console.log('  Examples:')
+    console.log('')
+    console.log(chalk.gray('    $ ') +
+                chalk.magenta('dong init spa') +
+                chalk.gray(' ....... Single Page Application'))
+    console.log(chalk.gray('    $ ') +
+                chalk.magenta('dong init web') +
+                chalk.gray(' ....... General Web Project'))
+    console.log('')
+  }
 }
